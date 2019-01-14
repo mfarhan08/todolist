9b5f5fce8db04973ca86682dc8840520f9832089
abhishek <abhishek.kumar@dunzo.in>
2019-01-14 22:41:17 +0530

    micro_go

--- model/Database.go
+++ model/Database.go
@@ -11,7 +11,10 @@ var database *sql.DB
 
 func Connect() error {
 
-	database, _ = sql.Open("mysql", "root:neosteam@tcp(127.0.0.1:3306)/todolist")
+	//database, _ = sql.Open("mysql", "abhishek:neosteam@tcp(192.168.1.210:3306)/todolist")
+
+	database, _ = sql.Open("mysql", "root:focus@tcp(127.0.0.1:3306)/todolist")
+	
 	statement, err := database.Prepare(TodoItemTable)
 	fmt.Println(err)
 	statement.Exec()
